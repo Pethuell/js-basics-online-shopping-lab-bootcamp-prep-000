430232a64c63d44da2f3c935f2fcd2ed06c1744f
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-08-01 20:47:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function removeFromCart(item) {
   var found = false;
   var i = 1;
   while(found === false && i < cart.length){
-    if(cart[i].itemName === item){
+    if(cart[i-1].itemName === item){
         cart.splice(i-1,i+1);
         found = true;
         return cart;
